48be423426e4803b369c349f5eb7b260234b4eb6
lakshmi2812 <laksmi.nishthala@gmail.com>
2020-05-16 14:14:00 -0700

    Rotate Array second attempt

--- leetcode-problems/RotateArray.java
+++ leetcode-problems/RotateArray.java
@@ -1,14 +1,16 @@
 class RotateArray {
     public void rotate(int[] nums, int k) {
-        //firt loop - iterate k times
-        for(int i = 0; i <= 2; i++){
-            //inner loop - iterate nums.length - 1 - i times. i starts from 1
-            int x = nums[i];
-            for(int j = i+1; j < nums.length-1-i; j++){
-                int temp = nums[j];
+        //outer loop - iterate k times
+        for(int t = 0; t <= k-1; t++){
+            //inner loop - iterate nums.length times. j starts from 1
+            int x = nums[t];
+            int temp = 0;
+            for(int j = t+1; j < nums.length; j++){
+                temp = nums[j];
                 nums[j] = x;
                 x = temp;
             }
+            nums[t] = temp;
         }
     }
 }
