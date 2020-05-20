fbbf1e8930c3bd14abeade3d22fe6cefac59708d
SiriD <sirisha.66@gmail.com>
2020-05-20 09:01:43 -0500

    one pass

--- leetcode-problems/BuyAndSell.java
+++ leetcode-problems/BuyAndSell.java
@@ -1,46 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class BuyAndSell {
 
     public static void main(String[] args) {
-        List<Integer> list1 = new ArrayList<>();
-        list1.add(7);
-        list1.add(6);
-        list1.add(4);
-        list1.add(3);
-        list1.add(1);
-        System.out.println(buyAndSell(list1));
+        int[] arr1  = {7,1,5,3,6,4};
+        System.out.println(buyAndSellOnepPass(arr1));
 
-        List<Integer> list2 = new ArrayList<>();
-        list2.add(7);
-        list2.add(1);
-        list2.add(5);
-        list2.add(3);
-        list2.add(6);
-        list2.add(4);
-        System.out.println(buyAndSell(list2));
+        int[] arr2 = {7,6,4,3,1};
+        System.out.println(buyAndSellOnepPass(arr2));
 
     }
 
-    static int buyAndSell(List<Integer> integerList) {
-        // arr = [7,1,5,3,6,4]
-        // profits[]
-        // for (0 < i < arr.length)
-        //      for (1 < j < arr.length)
-        //          if(arr[i] < arr[j])
-        //              profits{arr[j] - arr[i]}
-        // get the max of profits
+//    Brute force:
+//    static int buyAndSell(List<Integer> integerList) {
+//
+//        int maxProfit = 0;
+//        for(int i = 0; i < integerList.size(); i++) {
+//            for(int j = i + 1; j < integerList.size(); j++) {
+//                if((integerList.get(j) - integerList.get(i)) > maxProfit) {
+//                    maxProfit = integerList.get(j) - integerList.get(i);
+//                }
+//            }
+//        }
+//
+//        return maxProfit;
+//    }
+
+//  One pass solution
+    static int buyAndSellOnepPass(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-        for(int i = 0; i < integerList.size(); i++) {
-            for(int j = i + 1; j < integerList.size(); j++) {
-                if((integerList.get(j) - integerList.get(i)) > maxProfit) {
-                    maxProfit = integerList.get(j) - integerList.get(i);
-                }
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
             }
         }
 
         return maxProfit;
     }
-
 }
