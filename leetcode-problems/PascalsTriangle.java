405fcf54a807cce34b6578429b820b037931e1b7
lakshmi2812 <laksmi.nishthala@gmail.com>
2020-06-02 21:24:52 -0700

    Working solution for Pascals Triangle

---
+++ leetcode-problems/PascalsTriangle.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(numRows <= 0){
+            return result;
+        }
+        
+        for(int i = 0; i < numRows; i++){
+          List<Integer> temp = new ArrayList<Integer>();
+          for(int j = 0; j < i+1; j++){
+             if(j == 0 || j == i){
+                 temp.add(1);
+             }else{
+                 temp.add(result.get(i-1).get(j-1)+result.get(i-1).get(j));
+             }
+          }
+          result.add(temp); 
+        }
+        return result;
+    }
+}
